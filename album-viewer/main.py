660265dc08f52d611875503d09299b1e9478f178
cmack11 <claymackenthun@yahoo.com>
2022-02-19 15:57:18 -0600

    wip

--- album-viewer/main.py
+++ album-viewer/main.py
@@ -51,6 +51,7 @@ def get_current_track():
 def main():
 	current_track_id = None
 
+	spManager = SpotifyAssetManager()
 	# Configuration for the matrix
 	options = RGBMatrixOptions()
 	options.rows = 32
@@ -76,24 +77,21 @@ def main():
 				matrix.SetPixel(i,j,c[0], c[1], c[2])
 
 	while True:
-		try:
-			current_track_info = get_current_track()
-		except Exception:
-			continue
-		if current_track_info['id'] and current_track_info['id'] != current_track_id:
-			pprint(
-				current_track_info,
-				indent=4,
-			)
-			current_track_id = current_track_info['id']
-			album_url = current_track_info['album_cover_art']
-			response = requests.get(album_url)
-			img = Image.open(BytesIO(response.content))
+		if spManager.getTrackId() and spManger.getTrackId() != current_track_id:
+			#pprint(
+				#current_track_info,
+				#indent=4,
+			#)
+			print('setting', spManager.getTrackId())
+			current_track_id = spManager.getTrackId()
+			img = spManager.getImage()
+			if img == None:
+				break;
 			# Make image fit our screen.
 			img.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 			matrix.SetImage(img.convert('RGB'))
-		elif not current_track_info or not current_track_info['id']:
-			matrix.SetImage(fallback_img)
+		#elif not current_track_info or not current_track_info['id']:
+			#matrix.SetImage(fallback_img)
 		time.sleep(1)
 
 
