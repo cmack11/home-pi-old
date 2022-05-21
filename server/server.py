7313fc7148aa845bb2aef75e4241b6a4290bde85
cmack11 <claymackenthun@yahoo.com>
2022-05-20 20:06:53 -0500

    setup files

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 import asyncio
 import websockets
+import os
+import signal
 
 async def echo(websocket):
     print(websocket)
@@ -7,7 +9,13 @@ async def echo(websocket):
         await websocket.send(message)
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop run forever
 
 asyncio.run(main())
